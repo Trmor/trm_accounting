24ddb3afd39ea06399900a9bad2704d80875f6b0
mechanicerjr1 <krokodil.jr@yandex.ru>
2023-06-12 22:32:52 +0400

    added groupController

---
+++ src/groupController.ts
@@ -0,0 +1,30 @@
+import { IMap } from "./IMap";
+
+class Group {
+    name: string;
+
+    constructor(name: string){
+        this.name = name;
+    }
+}
+
+class groupController extends IMap {
+    
+    public static create(name: string): void {
+        let group: Group = new Group(name);
+        this.container.push(group);
+    }
+
+    public static edit(id: number, name:string): void {
+        let temp: Group = this.container.at(id);
+        temp.name = name;
+    };
+
+    public static getById(id: number) {
+        let group : Group = this.container.at(id);
+        return group;
+    };
+
+    protected static container: Array<Group> = new Array<Group>;
+
+}
